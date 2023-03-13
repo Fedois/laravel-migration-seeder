19ef7d33c31847265dd09a480731a7acf1752d7f
Fedois <fedefox14@gmail.com>
2023-03-13 16:30:16 +0100

    aggiunto app.blade.php

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>database Trains</title>
+
+        {{-- Includiamo gli assets con la direttiva @vite --}}
+        @vite('resources/js/app.js')
+    </head>
+    <body>
+
+        <main>
+            @yield('contentMain')
+        </main>
+
+    </body>
+</html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,56 +1,44 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts/app')
 
-        <title>database Trains</title>
+@section('contentMain')
+    @foreach ($trains as $train)
+        <div class="cards p-4">
+            <div class="border p-3">
+                <h4>nome del treno</h4>
+                <p>{{ $train->azienda }}</p>
 
-        {{-- Includiamo gli assets con la direttiva @vite --}}
-        @vite('resources/js/app.js')
-    </head>
-    <body>
+                <h4>stazione di partenza</h4>
+                <p>{{ $train->stazione_partenza }}</p>
 
-        <main class="">
-            @foreach ($trains as $train)
-                <div class="border p-5">
-                    <h4>nome del treno</h4>
-                    <p>{{ $train->azienda }}</p>
+                <h4>stazione di arrivo</h4>
+                <p>{{ $train->stazione_arrivo }}</p>
 
-                    <h4>stazione di partenza</h4>
-                    <p>{{ $train->stazione_partenza }}</p>
+                <h4>orario partenza</h4>
+                <p>{{ $train->orario_partenza }}</p>
 
-                    <h4>stazione di arrivo</h4>
-                    <p>{{ $train->stazione_arrivo }}</p>
+                <h4>orario arrivo</h4>
+                <p>{{ $train->orario_arrivo }}</p>
 
-                    <h4>orario partenza</h4>
-                    <p>{{ $train->orario_partenza }}</p>
+                <h4>codice</h4>
+                <p>{{ $train->codice_treno }}</p>
 
-                    <h4>orario arrivo</h4>
-                    <p>{{ $train->orario_arrivo }}</p>
+                <h4>numero carrozze</h4>
+                <p>{{ $train->n_carrozze }}</p>
 
-                    <h4>codice</h4>
-                    <p>{{ $train->codice_treno }}</p>
+                <h4>in orario</h4>
+                @if ($train->in_orario)
+                <p>si</p>
+                @else
+                <p>no</p>
+                @endif
 
-                    <h4>numero carrozze</h4>
-                    <p>{{ $train->n_carrozze }}</p>
-
-                    <h4>in orario</h4>
-                    @if ($train->in_orario)
-                    <p>si</p>
-                    @else
-                    <p>no</p>
-                    @endif
-
-                    <h4>cancellato</h4>
-                    @if ($train->cancellato)
-                    <p>si</p>
-                    @else
-                    <p>no</p>
-                    @endif
-                </div>
-            @endforeach
-        </main>
-
-    </body>
-</html>
+                <h4>cancellato</h4>
+                @if ($train->cancellato)
+                <p>si</p>
+                @else
+                <p>no</p>
+                @endif
+            </div>
+        </div>
+    @endforeach
+@endsection
